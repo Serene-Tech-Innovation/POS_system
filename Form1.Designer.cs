5740800f19cfaede7a69f4464a8c6711f1b31d56
Aashish Bhattarai <apandey9860@gmail.com>
2025-05-12 15:06:27 +0545

    Version Update

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace POS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cartListBox = new System.Windows.Forms.ListBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.flowPanelItems = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // cartListBox
+            // 
+            this.cartListBox.FormattingEnabled = true;
+            this.cartListBox.ItemHeight = 16;
+            this.cartListBox.Location = new System.Drawing.Point(1126, 97);
+            this.cartListBox.Name = "cartListBox";
+            this.cartListBox.Size = new System.Drawing.Size(350, 436);
+            this.cartListBox.TabIndex = 10;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Location = new System.Drawing.Point(1126, 559);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(243, 48);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "Total: Rs.0";
+            this.lblTotal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // flowPanelItems
+            // 
+            this.flowPanelItems.AutoScroll = true;
+            this.flowPanelItems.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.flowPanelItems.Location = new System.Drawing.Point(12, 37);
+            this.flowPanelItems.Name = "flowPanelItems";
+            this.flowPanelItems.Size = new System.Drawing.Size(951, 670);
+            this.flowPanelItems.TabIndex = 15;
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnCheckout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCheckout.Location = new System.Drawing.Point(1129, 598);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(132, 52);
+            this.btnCheckout.TabIndex = 16;
+            this.btnCheckout.Text = "Checkout";
+            this.btnCheckout.UseVisualStyleBackColor = false;
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox1.Location = new System.Drawing.Point(1126, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 63);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "Cart Items";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.Red;
+            this.clear.Location = new System.Drawing.Point(1364, 598);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(112, 52);
+            this.clear.TabIndex = 19;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.BackColor = System.Drawing.Color.Red;
+            this.btnRemoveItem.Location = new System.Drawing.Point(1364, 484);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(93, 33);
+            this.btnRemoveItem.TabIndex = 20;
+            this.btnRemoveItem.Text = "Remove";
+            this.btnRemoveItem.UseVisualStyleBackColor = false;
+            this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1601, 878);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.flowPanelItems);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.cartListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox cartListBox;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelItems;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button btnRemoveItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+using System.Text;
+using Npgsql;
+
+namespace POS
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, decimal> products = new Dictionary<string, decimal>();
+        Dictionary<string, string> productImages = new Dictionary<string, string>();
+
+        private void LoadProductsFromDB()
+        {
+            string connString = "Host=localhost;Username=admin;Password=tooshort;Database=POS-Sys_Def";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                using (var cmd = new NpgsqlCommand("SELECT name, price, image_path FROM products", conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(0);
+                        decimal price = reader.GetDecimal(1);
+                        string imagePath = reader.IsDBNull(2) ? "" : reader.GetString(2);
+                        products[name] = price;
+                        productImages[name] = imagePath;
+                    }
+                }
+            }
+        }
+
+        // Quantity tracker
+        Dictionary<string, int> cartQuantities = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProductsFromDB();   //Load from Postgres DB
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+                var productPanel = new Panel
+                {
+                    Size = new Size(150, 200),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Margin = new Padding(10)
+                };
+
+                var productLabel = new Label
+                {
+                    Text = product.Key,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Dock = DockStyle.Top,
+                    Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                    Height = 40
+                };
+
+                var productImage = new PictureBox
+                {
+                    Dock = DockStyle.Top,
+                    Height = 80,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    SizeMode = PictureBoxSizeMode.StretchImage
+                };
+
+                //string imagePath = Path.Combine("../../../Images", product.Key.ToLower().Replace(" ", "_") + ".jpg"); //For Dict Only Load
+                string imagePath = Path.Combine("../../../", productImages[product.Key]); //For Postgres DB Load
+
+                if (File.Exists(imagePath))
+                {
+                    productImage.Image = Image.FromFile(imagePath);
+                }
+                else
+                {
+                    productImage.BackColor = Color.LightGray;
+                    productImage.Image = null;
+                }
+
+                var productPrice = new Label
+                {
+                    Text = $"Rs. {product.Value}",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Dock = DockStyle.Top,
+                    Font = new Font("Arial", 18, FontStyle.Bold),
+                    Padding = new Padding(0, 5, 0, 0),
+                    Height = 30
+                };
+
+                var addButton = new Button
+                {
+                    Text = "Add to Cart",
+                    Height = 30,
+                    Dock = DockStyle.Bottom,
+                    Tag = product.Key
+                };
+
+                addButton.Click += (sender, e) =>
+                {
+                    string productName = ((Button)sender).Tag.ToString();
+
+                    if (cartQuantities.ContainsKey(productName))
+                    {
+                        cartQuantities[productName]++;
+                    }
+                    else
+                    {
+                        cartQuantities[productName] = 1;
+                    }
+
+                    UpdateCartDisplay();
+                };
+
+                productPanel.Controls.Add(addButton);
+                productPanel.Controls.Add(productPrice);
+                productPanel.Controls.Add(productImage);
+                productPanel.Controls.Add(productLabel);
+
+                flowPanelItems.Controls.Add(productPanel);
+            }
+        }
+
+        private void UpdateCartDisplay()
+        {
+            cartListBox.Items.Clear();
+            cartListBox.Items.Add(string.Format("{0,-15} {1,16} {2,25}", "Item", "Qty", "Price"));
+
+            foreach (var item in cartQuantities)
+            {
+                string name = item.Key;
+                int quantity = item.Value;
+                decimal unitPrice = products[name];
+                decimal subtotal = unitPrice * quantity;
+
+                string line = string.Format("{0,-20} {1,10} Rs.{2,10}", name, quantity, subtotal);
+                cartListBox.Items.Add(line);
+            }
+
+            UpdateTotal();
+        }
+
+        private void UpdateTotal()
+        {
+            decimal total = 0;
+
+            foreach (var item in cartQuantities)
+            {
+                if (products.ContainsKey(item.Key))
+                {
+                    total += products[item.Key] * item.Value;
+                }
+            }
+
+            lblTotal.Text = $"Total: Rs. {total}";
+        }
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            if (cartQuantities.Count == 0)
+            {
+                MessageBox.Show("Your cart is empty.");
+                return;
+            }
+
+            StringBuilder receipt = new StringBuilder();
+            decimal total = 0;
+
+            receipt.AppendLine("Items in Cart:\n");
+
+            foreach (var item in cartQuantities)
+            {
+                string productName = item.Key;
+                int quantity = item.Value;
+
+                if (products.ContainsKey(productName))
+                {
+                    decimal price = products[productName];
+                    decimal subtotal = price * quantity;
+                    total += subtotal;
+                    receipt.AppendLine($"{productName} x{quantity} - Rs. {subtotal}");
+                }
+            }
+
+            receipt.AppendLine($"\nTotal: Rs. {total}");
+
+            MessageBox.Show(receipt.ToString(), "Checkout Summary");
+
+            // Clear the cart
+            cartQuantities.Clear();
+            UpdateCartDisplay();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //LoadProductsFromDB();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Total_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCheckout_Click_1(object sender, EventArgs e)
+        {
+            btnCheckout_Click(sender, e);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            cartListBox.Items.Clear();
+            lblTotal.Text = "Total: Rs. 0";
+        }
+
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            if (cartListBox.SelectedIndex > 0)
+            {
+                string selected = cartListBox.SelectedItem.ToString();
+
+
+                string itemName = selected.Substring(0, 20).Trim();
+
+                if (cartQuantities.ContainsKey(itemName))
+                {
+                    if (cartQuantities[itemName] > 1)
+                        cartQuantities[itemName]--;
+                    else
+                        cartQuantities.Remove(itemName);
+
+                    UpdateCartDisplay();
+                }
+            }
+        }
+    }
+}
