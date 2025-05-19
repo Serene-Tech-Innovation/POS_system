cb11293f2c872eec36446b3609019a00b0199650
Aashish Bhattarai <apandey9860@gmail.com>
2025-05-19 17:38:02 +0545

    Add category and subcategory selection to product edit
    Updated `QuickProductEditWindow.xaml` to include ComboBoxes for selecting a product's category and subcategory. Introduced `LoadProductToWindow` method in `QuickProductEditWindow.xaml.cs` to populate UI fields with product data, ensuring the interface reflects the selected product's details upon loading.

--- Dialogs/QuickProductEditWindow.xaml.cs
+++ Dialogs/QuickProductEditWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POS.Models.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,34 @@ namespace POS
     /// </summary>
     public partial class QuickProductEditWindow : Window
     {
-        public QuickProductEditWindow()
+
+        private void LoadProductToWindow(Product product)
+        {
+            if (product == null) return;
+            NameBox.Text = product.Name;
+            PriceBox.Text = product.Price.ToString();
+            StockBox.Text = product.Stock.ToString();
+            if (product.Category != null)
+            {
+                foreach (var category in ProductDataStore.Categories)
+                {
+                    CategoryBox.Items.Add(category.Name);
+                }
+                foreach (var subcategory in product.Category.Subcategories)
+                {
+                    SubCategoryBox.Items.Add(subcategory.Name);
+                }
+                //CategoryBox.ItemsSource = ProductDataStore.Categories;
+                CategoryBox.SelectedItem = product.Category.Name;
+                //SubCategoryBox.ItemsSource = product.Category.Subcategories;
+                SubCategoryBox.SelectedIndex = SubCategoryBox.Items.IndexOf(product.Subcategory.Name);
+            }
+        }
+
+        public QuickProductEditWindow(Product product)
         {
             InitializeComponent();
+            LoadProductToWindow(product);
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
